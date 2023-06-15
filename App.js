f70aa835d9af387b1b7e69b7edb02061a502363c
munij3 <a01781341@tec.mx>
2023-06-15 12:26:13 -0600

    Lambda

--- App.js
+++ App.js
@@ -17,4 +17,18 @@ function App({ signOut, user }) {
     );
 }
 
+const { handler } = require('./lambda');
+
+exports.main = async (event, context) => {
+  try {
+    const response = await handler(event, context);
+    return response;
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
+
 export default WithAuthenticator(App);
